2880b17913514f826d51f5a2d2b17c4d6bb39f1e
davidoberst <juandavidbustosperez82@gmail.com>
2026-07-17 15:34:48 -0500

    Set up LED pins in binary_counter
    Initialize LED pins for binary counter.

---
+++ binary_counter.cpp
@@ -0,0 +1,19 @@
+void setup() {
+
+ int led1 = 5;
+ int led2 = 4;
+ int led3 = 3;
+ int led2 = 2;
+
+ pinMode(led1, OUTPUT);
+ pinMode(led2, OUTPUT);
+ pinMode(led3, OUTPUT);
+ pinMode(led4, OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
