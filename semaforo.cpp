76d501b9bb437bcc26e63c809d764abfb270a383
davidoberst <juandavidbustosperez82@gmail.com>
2026-07-23 14:05:39 -0500

    Add button for traffic light control
    Added button input handling for traffic light control.

--- semaforo.cpp
+++ semaforo.cpp
@@ -1,9 +1,12 @@
 int redLED = 8;
 int yellowLED = 4;
 int greenLED = 2;
+int button = 7;
+int buttonStatus = digitalRead(button);
 
 
 void setup() {
+  pinMode(button, INPUT);
   pinMode(redLED,OUTPUT);
   pinMode(yellowLED,OUTPUT);
   pinMode(greenLED,OUTPUT);
@@ -13,29 +16,26 @@ void loop() {
  //ciclo de semaforo
   
   //encender rojo
-  
   digitalWrite(redLED,HIGH);
   delay(8000);
   digitalWrite(redLED,LOW); //esperar 3 segundos y luego pasar a amarillo
    
 
-  
   digitalWrite(yellowLED,HIGH);
   delay(4000);
   digitalWrite(yellowLED,LOW);
 
-
-    
+   
   digitalWrite(greenLED,HIGH);
   delay(8000);
   digitalWrite(greenLED,LOW);
   
-
-
-
-
-
-
+  if(buttonStatus == HIGH){
+   
+  }else{
+    
+  }
 
   
 }
+
