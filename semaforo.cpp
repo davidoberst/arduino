62d811863ea5a58bd92e4203e3f6633ec55fc0dc
davidoberst <juandavidbustosperez82@gmail.com>
2026-07-23 16:12:29 -0500

    Improve LED control and add smart green LED feature
    Refactor LED control logic and add intelligent green LED functionality.

--- semaforo.cpp
+++ semaforo.cpp
@@ -3,44 +3,49 @@ int yellowLED = 4;
 int greenLED = 2;
 int button = 7;
 
-
-
 void setup() {
   pinMode(button, INPUT);
-  pinMode(redLED,OUTPUT);
-  pinMode(yellowLED,OUTPUT);
-  pinMode(greenLED,OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
 }
 
 void loop() {
   int buttonStatus = digitalRead(button);
-  if(buttonStatus == HIGH){
-      digitalWrite(greenLED,LOW);
-      digitalWrite(yellowLED,HIGH);
-      delay(2000);
-      digitalWrite(yellowLED,LOW);
-      digitalWrite(redLED,HIGH);
-      delay(10000);
-      digitalWrite(redLED, LOW);
 
-  }else{
-     digitalWrite(redLED,HIGH);
-     delay(5000);
-     digitalWrite(redLED,LOW); //esperar 3 segundos y luego pasar a amarillo
-   
-
-     digitalWrite(yellowLED,HIGH);
-     delay(5000);
-     digitalWrite(yellowLED,LOW);
-
-   
-     digitalWrite(greenLED,HIGH);
-     delay(20000);
-     digitalWrite(greenLED,LOW);  
+  if (buttonStatus == HIGH) {
+    digitalWrite(greenLED, LOW);
     
+    digitalWrite(yellowLED, HIGH);
+    delay(2000);
+    digitalWrite(yellowLED, LOW);
+    
+    digitalWrite(redLED, HIGH);
+    delay(10000);
+    digitalWrite(redLED, LOW);
+
+  } else {
+  
+    digitalWrite(redLED, HIGH);
+    delay(5000);
+    digitalWrite(redLED, LOW);
+
+    digitalWrite(yellowLED, HIGH);
+    delay(5000);
+    digitalWrite(yellowLED, LOW);
+
+    // --- VERDE INTELIGENTE (20 segundos = 200 iteraciones de 100ms) ---
+    digitalWrite(greenLED, HIGH);
+    
+    for (int i = 0; i < 200; i++) {
+      delay(100); 
+      
+      
+      if (digitalRead(button) == HIGH) {
+        break; 
+      }
+    }
+    
+    digitalWrite(greenLED, LOW);  
   }
-  //encender rojo
-
 }
-
-
