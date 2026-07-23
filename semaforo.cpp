8e215fd8ae7d114e21275bdcf71d2caa479a0500
davidoberst <juandavidbustosperez82@gmail.com>
2026-07-22 20:40:16 -0500

    Add yellow and green LED functionality

--- semaforo.cpp
+++ semaforo.cpp
@@ -1,11 +1,16 @@
 int redLED = 13;
-
+int yellowLED = 12;
+int greenLED = 8;
 
 
 void setup() {
   pinMode(redLED,OUTPUT);
+  pinMode(yellowLED,OUTPUT);
+  pinMode(greenLED,OUTPUT);
 }
 
 void loop() {
   digitalWrite(redLED,HIGH);
+  digitalWrite(yellowLED,HIGH);
+  digitalWrite(greenLED,HIGH);
 }
